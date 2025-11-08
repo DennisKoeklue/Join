311b8105751ad787382338c0b61810821f6c467a
LarsSchoenemannDev <lars.dev90@gmail.com>
2025-11-08 13:15:06 +0100

    changes for add task

---
+++ javascript/add-task.js
@@ -0,0 +1,11 @@
+let addTaskTemp = []
+
+/**
+ * This function is used .....
+ * 
+ */
+function addTaskCheak(){
+    let titleTask = document.getElementById("title").value;
+    addTaskTemp.push(titleTask)
+    console.log(addTaskTemp);    
+}
