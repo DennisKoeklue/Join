e478a01fca8101fe36b83d514984c832fab7beee
Dennis Köklü <dennis.koeklue@gmail.com>
2025-11-12 21:22:42 +0100

    added Script data for Sidebar and Header, changes on Header.css and privacy-policy.html

---
+++ JS/sidebar_and_header_script.js
@@ -0,0 +1,10 @@
+
+//Hier bitte alle globale Variablen setzen
+let user_icon = document.getElementById('user-icon')
+
+
+function tooglemenu() {
+    let msg = document.getElementById('dropDown-menu')
+    msg.classList.toggle('show')
+}
+
